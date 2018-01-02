97d2186b0720d1a01c6cc0b8f809a9c142e7dce8
Irene DeVera <irenneeyyy@gmail.com>
2018-01-02 11:55:12 -0800

    Initial Commit

---
+++ TheNewMeta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheNewMeta
+//
+//  Created by Irene DeVera on 1/2/18.
+//  Copyright © 2018 Irene DeVera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
