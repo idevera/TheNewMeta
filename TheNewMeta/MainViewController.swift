47f922fbbe4595b2c10855628e497537f9990f3e
Irene DeVera <irenneeyyy@gmail.com>
2018-01-05 12:39:56 -0800

    Added all game titles and appended them to the table cells

--- TheNewMeta/MainViewController.swift
+++ TheNewMeta/MainViewController.swift
@@ -11,8 +11,11 @@ import RealmSwift
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    // TODO: Does this need to be a constant?
+    var currentGames = [String]()
+    
     @IBOutlet weak var tableViewContent: UITableView!
-    let testData = ["hello", "goodbye", "salut"]
+//        let testData = ["hello", "goodbye", "salut"]
     
     @IBAction func clickHamburger() {
         print("TOGGLE SIDE MENU")
@@ -20,7 +23,17 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         NotificationCenter.default.post(name: NSNotification.Name("ToggleSideMenu"), object: nil)
     }
     
+    func getData() {
+        let realm = try! Realm()
+        let returnedGames = realm.objects(Game.self)
+        print("This is all the games in the DB \(returnedGames.count)")
+        for game in returnedGames {
+            currentGames.append(game.title)
+        }
+    }
+    
     // Lobbies Table
+    
     // How many sections in your table
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -28,38 +41,33 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     // How many rows in your table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        let realm = try! Realm()
-//        let allGames = realm.objects(Game.self)
-        print("Number of games currently in my db: \(testData.count)")
-        return testData.count
+        return currentGames.count
     }
     
     // What are the contents of each cell?
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "gameTitleCell")
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "gameTitleCell”, for: indexPath)
-        //        func dequeueReusableCell(withIdentifier identifier: String,
-//                                 for indexPath: IndexPath) -> UITableViewCell
-//        print("This is a table cell: ")
         print("This is the textLabel:")
         // The indexPath moves down the array and prints each string in their own row
-        cell.textLabel?.text = testData[indexPath.row]
+        cell.textLabel?.text = currentGames[indexPath.row]
         // Return the cell!
         return cell
     }
     
+
     
-//    Lobby {
-//    lobbyID = 4A362BCD-97C8-4D02-BE37-E314869DF7D1;
-//    hostID = 421F7DC6-EF38-4A7E-BB0E-8CEB1D6347AD;
-//    game = Game {
-//    gameID = 1AFB77B7-1DEB-4278-BF93-042DAAE992F1;
-//    title = NotOverWatch;
-//    };
-//    numberOfPlayers = 1;
-//    message = Looking for you!;
-//    }
-    //
+    
+    //    Lobby {
+    //    lobbyID = 4A362BCD-97C8-4D02-BE37-E314869DF7D1;
+    //    hostID = 421F7DC6-EF38-4A7E-BB0E-8CEB1D6347AD;
+    //    game = Game {
+    //    gameID = 1AFB77B7-1DEB-4278-BF93-042DAAE992F1;
+    //    title = NotOverWatch;
+    //    };
+    //    numberOfPlayers = 1;
+    //    message = Looking for you!;
+    //    }
+        //
     
     
     // Will be consumed by the NC --> Messages are sent from the sideMenuViewController on click
@@ -75,21 +83,24 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         performSegue(withIdentifier: "ShowEditProfile", sender: nil)
     }
     
-//    @objc func showLoginView () {
-//        performSegue(withIdentifier: "ShowLoginView", sender: nil)
-//    }
+    //    @objc func showLoginView () {
+    //        performSegue(withIdentifier: "ShowLoginView", sender: nil)
+    //    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getData()
+        
         tableViewContent.delegate = self
         tableViewContent.dataSource = self
+        
         
         // NAVIGATION
         // On load, add the observers to the NC to be listening for a click event!!!
         NotificationCenter.default.addObserver(self, selector: #selector(showCreateLobby), name: NSNotification.Name("ShowCreateLobby"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(showMyLobbies), name: NSNotification.Name("ShowMyLobbies"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(showEditProfile), name: NSNotification.Name("ShowEditProfile"), object: nil)
-        //        NotificationCenter.default.addObserver(self, selector: #selector(showLoginView), name: NSNotification.Name("ShowLoginView"), object: nil)
+        // NotificationCenter.default.addObserver(self, selector: #selector(showLoginView), name: NSNotification.Name("ShowLoginView"), object: nil)
         
         
     }
