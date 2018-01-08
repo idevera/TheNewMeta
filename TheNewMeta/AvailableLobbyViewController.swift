e8f3d04ddff9a2d3559f99650ff505dd4a890132
Irene DeVera <irenneeyyy@gmail.com>
2018-01-08 14:44:06 -0800

    Figured out that integers can can never be nil. Thus an optional is required to be given

--- TheNewMeta/AvailableLobbyViewController.swift
+++ TheNewMeta/AvailableLobbyViewController.swift
@@ -10,28 +10,80 @@ import UIKit
 import Foundation
 import RealmSwift
 
-class AvailableLobbyViewController: UIViewController {
+class AvailableLobbyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var chosenGame: Game?
-    // This should be an array of all lobbies
-    var gameLobbies = [Lobby]()
     
-    private func getMatchingLobbies() {
-//        let realm = try! Realm()
-        
-        // In product, look for any property of productTypelist.typeName that is equal to %@ --> productTypeName = "electronics"
-//        realm.objects(Product.self).filter("ANY productTypeList.typeName == %@", productTypeName)
-//        let returnedLobbies = realm.objects(Lobby.self).filter("ANY game = chosenGame")
-//        print("This is all the games in the DB \(returnedLobbies.count)")
-//        for lobby in returnedLobbies {
-//            gameLobbies.append(lobby)
-//        }
+    let lobbyCellIdentifier = "LobbyTableCell"
+    
+    @IBOutlet weak var LobbyTableView: UITableView!
+    
+    // Lobby Tables
+    
+    // How many sections in your table
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
+    
+    // How many rows in your table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chosenGame!.matchingLobbies.count
+    }
+    
+    // What are the contents of each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // The as? MealTableViewCell expression attempts to downcast the returned object from the UITableViewCell class to your MealTableViewCell class. This returns an optional.
+        // The guard let expression safely unwraps the optional.
+        // If your storyboard is set up correctly, and the cellIdentifier matches the identifier from your storyboard, then the downcast should never fail. If the downcast does fail, the fatalError() function prints an error message to the console and terminates the app.
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: lobbyCellIdentifier, for: indexPath) as? LobbyViewCell else {
+            fatalError("The dequeued cell is not an instance of GameTableViewCell.")
+        }
+        
+        // The table view will iterate over each thing in the matchingLobbies
+        let lobby = chosenGame?.matchingLobbies[indexPath.row]
+        
+        
+//        The text of your textField is an optional value because a text field can sometimes be empty. So, use an if let to unwrap the variable and make sure it's not nil — otherwise your program could crash:
+        cell.gamerTagLabel.text = lobby?.hostID
+        
+//        print("Value is \(optionalValue as Int?)")
+        let players = "\(lobby?.numberOfPlayers ?? 0)"
+        cell.numPlayersLabel.text = players
+        cell.messageLabel.text = lobby?.message
+        
+//        var myVariable = 1
+//
+//        myVariable += 1
+//        label.text = string(myVariable)
+        
+//        let x : Int = 42
+//        var myString = String(x)
+
+        //        @IBOutlet weak var gamerTagLabel: UILabel!
+//        @IBOutlet weak var numPlayersLabel: UILabel!
+//        @IBOutlet weak var messageLabel: UILabel!
+        
+//        @objc dynamic var lobbyID: String = UUID().uuidString
+//        @objc dynamic var hostID: String = ""
+//        // Objects must be optional
+//        //    @objc dynamic var game: Game?
+//        @objc dynamic var numberOfPlayers: Int = 0
+//        @objc dynamic var message: String = ""
+//
+//        override static func primaryKey() -> String? {
+//            return "lobbyID"
+//        }
+        
+        // Return the cell view
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        getMatchingLobbies()
+        LobbyTableView.delegate = self
+        LobbyTableView.dataSource = self
         print("This is the avilableLobbyVC chosen game: \(String(describing: chosenGame))")
-        print("This is the gameLobbies that match this game: \(gameLobbies)")
+//        print("This is the gameLobbies that match this game: \(chosenGame?.matchingLobbies)")
 
         // Do any additional setup after loading the view.
     }
