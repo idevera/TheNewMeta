fa9c01cf60f6dc5e700265722b000e7430e4187d
Irene DeVera <irenneeyyy@gmail.com>
2018-01-16 14:39:15 -0800

    Fix constraints on text field views when creating a lobby

--- TheNewMeta/CreateLobbyViewController.swift
+++ TheNewMeta/CreateLobbyViewController.swift
@@ -15,18 +15,12 @@ import RealmSwift
 class CreateLobbyViewController: UIViewController, UITextFieldDelegate {
     
     private var signedInUser = User()
-    
-    // Outlets
-    
-    @IBOutlet weak var gameTagField: UITextField!
-    @IBOutlet weak var numberOfPlayersField: UITextField!
-    @IBOutlet weak var messageTagField: UITextField!
 
     // Actions
 
-    @IBAction func createLobby(_ sender: UIButton) {
+    @objc func createLobby(_ sender: UIButton) {
         // This game is either a new created game OR an existing game
-        let game = getGame(gameTitle: gameTagField.text!)
+        let game = getGame(gameTitle: gameFieldView.text!)
         let newLobby = createNewLobby()
         
         // Perform the migration
@@ -53,9 +47,9 @@ class CreateLobbyViewController: UIViewController, UITextFieldDelegate {
     private func createNewLobby() -> Lobby {
         let lobby = Lobby()
         
-        let num: Int? = Int(numberOfPlayersField.text!)
+        let num: Int? = Int(playersFieldView.text!)
         lobby.numberOfPlayers = num!
-        lobby.message = messageTagField.text!
+        lobby.message = msgFieldView.text!
         
         // It’s worth noting here that these getters will return optional values, so the type of name is String?. When the "name" key doesn’t exist, the code returns nil. It then makes sense to use optional binding to get the value safely:
         if let id = UserDefaults.standard.string(forKey: "userID") {
@@ -79,7 +73,7 @@ class CreateLobbyViewController: UIViewController, UITextFieldDelegate {
     
     private func createGame() -> Game {
         let newGame = Game()
-        newGame.title = gameTagField.text!
+        newGame.title = gameFieldView.text!
         return newGame
     }
     
@@ -97,7 +91,6 @@ class CreateLobbyViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.backgroundColor = .blue
         setupLayout()
         findSignedInUser()
     }
@@ -131,6 +124,17 @@ class CreateLobbyViewController: UIViewController, UITextFieldDelegate {
         msgField.placeholder = "Add a message to your challengers!"
         return msgField
     }()
+    
+    let submitButtonView: UIButton = {
+        let submitButton = UIButton()
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
+        submitButton.backgroundColor = .yellow
+        submitButton.layer.cornerRadius = 10
+        submitButton.setTitle("Create Lobby", for: .normal)
+        submitButton.setTitleColor(UIColor.darkGray, for: .normal)
+        submitButton.addTarget(self, action: #selector(createLobby(_:)), for: .touchUpInside)
+        return submitButton
+    }()
 
     private func setupLayout() {
         view.backgroundColor = UIColor(red:0.53, green:0.77, blue:0.80, alpha:1.0)
@@ -145,92 +149,18 @@ class CreateLobbyViewController: UIViewController, UITextFieldDelegate {
         playersFieldView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
         
         gameFieldView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        gameFieldView.bottomAnchor.constraint(equalTo: playersFieldView.topAnchor).isActive = true
-//        gameFieldView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        gameFieldView.bottomAnchor.constraint(equalTo: playersFieldView.topAnchor, constant: -20).isActive = true
         gameFieldView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
         gameFieldView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
         
         msgFieldView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        msgFieldView.topAnchor.constraint(equalTo: playersFieldView.bottomAnchor).isActive = true
-//        msgFieldView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        msgFieldView.topAnchor.constraint(equalTo: playersFieldView.bottomAnchor, constant: 20).isActive = true
         msgFieldView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
         msgFieldView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
 
-
-
-//        @IBOutlet weak var gameTagField: UITextField!
-//        @IBOutlet weak var numberOfPlayersField: UITextField!
-//        @IBOutlet weak var messageTagField: UITextField!
-
-//        view.addSubview(gameFieldView)
-//        view.addSubview(playersFieldView)
-//        view.addSubview(msgFieldView)
-
-//
-//        playersFieldView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        playersFieldView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-
-//        // Add the image to the container
-//        topImageContainerView.addSubview(joystickImageView)
-//
-//        // Joystick constraints
-//        joystickImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
-//        joystickImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
-//        joystickImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
-//
-//        // Title
-//
-//        topImageContainerView.addSubview(appTitleView)
-//        appTitleView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
-//        appTitleView.centerYAnchor.constraint(equalTo: joystickImageView.bottomAnchor, constant: 20).isActive = true
-//
-//        // Add Bottom View
-//
-//        let bottomViewContainer = UIView()
-//        // Uncomment if you want to see what it looks like
-//        bottomViewContainer.backgroundColor = UIColor(red:0.53, green:0.77, blue:0.80, alpha:1.0)
-//        // Add container to the view
-//        view.addSubview(bottomViewContainer)
-//
-//        bottomViewContainer.translatesAutoresizingMaskIntoConstraints = false
-//        // Makes it 50% of the views size
-//        bottomViewContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6).isActive = true
-//        bottomViewContainer.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
-//        // Instead of left and right anchor we use leading and trailing anchor.
-//        // It does the same thing. Video explains why we prefer it (convention)
-//        bottomViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        bottomViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//
-//        bottomViewContainer.addSubview(gamerTagView)
-//        bottomViewContainer.addSubview(emailTagView)
-//        bottomViewContainer.addSubview(pwTagView)
-//        bottomViewContainer.addSubview(loginButtonView)
-//
-//        // GamerTagView
-//
-//        gamerTagView.centerXAnchor.constraint(equalTo: bottomViewContainer.centerXAnchor).isActive = true
-//        gamerTagView.widthAnchor.constraint(equalTo: bottomViewContainer.widthAnchor, multiplier: 0.7).isActive = true
-//        gamerTagView.topAnchor.constraint(equalTo: bottomViewContainer.topAnchor, constant: 20).isActive = true
-//
-//        // Email View
-//
-//        emailTagView.centerXAnchor.constraint(equalTo: bottomViewContainer.centerXAnchor).isActive = true
-//        emailTagView.widthAnchor.constraint(equalTo: bottomViewContainer.widthAnchor, multiplier: 0.7).isActive = true
-//        emailTagView.topAnchor.constraint(equalTo: gamerTagView.bottomAnchor, constant: 20).isActive = true
-//
-//        // PW View
-//
-//        pwTagView.centerXAnchor.constraint(equalTo: bottomViewContainer.centerXAnchor).isActive = true
-//        pwTagView.widthAnchor.constraint(equalTo: bottomViewContainer.widthAnchor, multiplier: 0.7).isActive = true
-//        pwTagView.topAnchor.constraint(equalTo: emailTagView.bottomAnchor, constant: 20).isActive = true
-//
-//        // Login Button
-//
-//        loginButtonView.centerXAnchor.constraint(equalTo: bottomViewContainer.centerXAnchor).isActive = true
-//        loginButtonView.widthAnchor.constraint(equalTo: bottomViewContainer.widthAnchor, multiplier: 0.7).isActive = true
-//        loginButtonView.topAnchor.constraint(equalTo: pwTagView.bottomAnchor, constant: 40).isActive = true
-//        loginButtonView.setTitle("Login", for: .normal)
-//        loginButtonView.setTitleColor(UIColor.darkGray, for: .normal)
-//        loginButtonView.addTarget(self, action: #selector(signIn), for: .touchUpInside)
+        view.addSubview(submitButtonView)
+        submitButtonView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        submitButtonView.topAnchor.constraint(equalTo: msgFieldView.bottomAnchor, constant: 20).isActive = true
+        submitButtonView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
     }
 }
