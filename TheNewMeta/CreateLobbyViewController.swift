28be141ba57a6d01a4e1bfc67e9284c3ce50a65f
Irene DeVera <irenneeyyy@gmail.com>
2018-01-16 14:16:02 -0800

    Add styling to create lobby page

--- TheNewMeta/CreateLobbyViewController.swift
+++ TheNewMeta/CreateLobbyViewController.swift
@@ -97,7 +97,140 @@ class CreateLobbyViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        view.backgroundColor = .blue
+        setupLayout()
         findSignedInUser()
+    }
+    
+    let gameFieldView: UITextField = {
+        let gameField = UITextField()
+        gameField.translatesAutoresizingMaskIntoConstraints = false
+        gameField.backgroundColor = .white
+        gameField.layer.cornerRadius = 10
+        gameField.textAlignment = .center
+        gameField.placeholder = "Enter the name of your game"
+        return gameField
+    }()
+
+    let playersFieldView: UITextField = {
+        let playersField = UITextField()
+        playersField.translatesAutoresizingMaskIntoConstraints = false
+        playersField.backgroundColor = .white
+        playersField.layer.cornerRadius = 10
+        playersField.textAlignment = .center
+        playersField.placeholder = "Please enter the number of players"
+        return playersField
+    }()
+
+    let msgFieldView: UITextField = {
+        let msgField = UITextField()
+        msgField.translatesAutoresizingMaskIntoConstraints = false
+        msgField.backgroundColor = .white
+        msgField.layer.cornerRadius = 10
+        msgField.textAlignment = .center
+        msgField.placeholder = "Add a message to your challengers!"
+        return msgField
+    }()
+
+    private func setupLayout() {
+        view.backgroundColor = UIColor(red:0.53, green:0.77, blue:0.80, alpha:1.0)
+
+        view.addSubview(playersFieldView)
+        view.addSubview(gameFieldView)
+        view.addSubview(msgFieldView)
+        
+        playersFieldView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        playersFieldView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        playersFieldView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
+        playersFieldView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
+        
+        gameFieldView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        gameFieldView.bottomAnchor.constraint(equalTo: playersFieldView.topAnchor).isActive = true
+//        gameFieldView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        gameFieldView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
+        gameFieldView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
+        
+        msgFieldView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        msgFieldView.topAnchor.constraint(equalTo: playersFieldView.bottomAnchor).isActive = true
+//        msgFieldView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        msgFieldView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
+        msgFieldView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
+
+
+
+//        @IBOutlet weak var gameTagField: UITextField!
+//        @IBOutlet weak var numberOfPlayersField: UITextField!
+//        @IBOutlet weak var messageTagField: UITextField!
+
+//        view.addSubview(gameFieldView)
+//        view.addSubview(playersFieldView)
+//        view.addSubview(msgFieldView)
+
+//
+//        playersFieldView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        playersFieldView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+//        // Add the image to the container
+//        topImageContainerView.addSubview(joystickImageView)
+//
+//        // Joystick constraints
+//        joystickImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+//        joystickImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+//        joystickImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+//
+//        // Title
+//
+//        topImageContainerView.addSubview(appTitleView)
+//        appTitleView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+//        appTitleView.centerYAnchor.constraint(equalTo: joystickImageView.bottomAnchor, constant: 20).isActive = true
+//
+//        // Add Bottom View
+//
+//        let bottomViewContainer = UIView()
+//        // Uncomment if you want to see what it looks like
+//        bottomViewContainer.backgroundColor = UIColor(red:0.53, green:0.77, blue:0.80, alpha:1.0)
+//        // Add container to the view
+//        view.addSubview(bottomViewContainer)
+//
+//        bottomViewContainer.translatesAutoresizingMaskIntoConstraints = false
+//        // Makes it 50% of the views size
+//        bottomViewContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6).isActive = true
+//        bottomViewContainer.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+//        // Instead of left and right anchor we use leading and trailing anchor.
+//        // It does the same thing. Video explains why we prefer it (convention)
+//        bottomViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//        bottomViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+//
+//        bottomViewContainer.addSubview(gamerTagView)
+//        bottomViewContainer.addSubview(emailTagView)
+//        bottomViewContainer.addSubview(pwTagView)
+//        bottomViewContainer.addSubview(loginButtonView)
+//
+//        // GamerTagView
+//
+//        gamerTagView.centerXAnchor.constraint(equalTo: bottomViewContainer.centerXAnchor).isActive = true
+//        gamerTagView.widthAnchor.constraint(equalTo: bottomViewContainer.widthAnchor, multiplier: 0.7).isActive = true
+//        gamerTagView.topAnchor.constraint(equalTo: bottomViewContainer.topAnchor, constant: 20).isActive = true
+//
+//        // Email View
+//
+//        emailTagView.centerXAnchor.constraint(equalTo: bottomViewContainer.centerXAnchor).isActive = true
+//        emailTagView.widthAnchor.constraint(equalTo: bottomViewContainer.widthAnchor, multiplier: 0.7).isActive = true
+//        emailTagView.topAnchor.constraint(equalTo: gamerTagView.bottomAnchor, constant: 20).isActive = true
+//
+//        // PW View
+//
+//        pwTagView.centerXAnchor.constraint(equalTo: bottomViewContainer.centerXAnchor).isActive = true
+//        pwTagView.widthAnchor.constraint(equalTo: bottomViewContainer.widthAnchor, multiplier: 0.7).isActive = true
+//        pwTagView.topAnchor.constraint(equalTo: emailTagView.bottomAnchor, constant: 20).isActive = true
+//
+//        // Login Button
+//
+//        loginButtonView.centerXAnchor.constraint(equalTo: bottomViewContainer.centerXAnchor).isActive = true
+//        loginButtonView.widthAnchor.constraint(equalTo: bottomViewContainer.widthAnchor, multiplier: 0.7).isActive = true
+//        loginButtonView.topAnchor.constraint(equalTo: pwTagView.bottomAnchor, constant: 40).isActive = true
+//        loginButtonView.setTitle("Login", for: .normal)
+//        loginButtonView.setTitleColor(UIColor.darkGray, for: .normal)
+//        loginButtonView.addTarget(self, action: #selector(signIn), for: .touchUpInside)
     }
 }
