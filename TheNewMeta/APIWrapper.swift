8439131dcce11f38be36f559e4fc9c247c09a621
Irene DeVera <irenneeyyy@gmail.com>
2018-01-16 15:53:37 -0800

    Add API Wrapper class

---
+++ TheNewMeta/APIWrapper.swift
@@ -0,0 +1,13 @@
+//
+//  APIWrapper.swift
+//  TheNewMeta
+//
+//  Created by Irene DeVera on 1/16/18.
+//  Copyright © 2018 Irene DeVera. All rights reserved.
+//
+
+import Foundation
+
+class APIWrapper {
+    
+}
